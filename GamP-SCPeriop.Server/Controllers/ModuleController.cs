000a2ed2ae2c04085cf88cafdb4c2ac41b8304fb
SergiooOliveira <sergioooliveira908@gmail.com>
2026-03-15 11:30:07 +0000

    feat: add ModuleController and establish Pathway-Module relationship

---
+++ GamP-SCPeriop.Server/Controllers/ModuleController.cs
@@ -0,0 +1,27 @@
+﻿using GamP_SCPeriop.Server.Data;
+using GamP_SCPeriop.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamP_SCPeriop.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ModuleController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ModuleController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Module>> CreateModule(Module newModule)
+        {
+            _context.Modules.Add(newModule);
+            await _context.SaveChangesAsync();
+
+            return Ok(newModule);
+        }
+    }
+}
