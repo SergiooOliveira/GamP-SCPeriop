d741cf145ea59866b273bb3a201d992f9a99fbc5
SergiooOliveira <sergioooliveira908@gmail.com>
2026-03-15 12:31:49 +0000

    feat: add ModuleComponentController to complete the backend learning chain

---
+++ GamP-SCPeriop.Server/Controllers/ModuleComponentController.cs
@@ -0,0 +1,28 @@
+﻿using GamP_SCPeriop.Server.Data;
+using GamP_SCPeriop.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamP_SCPeriop.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ModuleComponentController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ModuleComponentController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ModuleComponent>> CreateComponent(ModuleComponent newModuleComponent)
+        {
+            _context.ModuleComponents.Add(newModuleComponent);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(newModuleComponent);
+        }
+    }
+}
