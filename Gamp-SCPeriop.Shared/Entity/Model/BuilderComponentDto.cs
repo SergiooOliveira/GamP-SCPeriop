6cfa919d24084c19c78e8f634956c9c31a533d46
SergiooOliveira <sergioooliveira908@gmail.com>
2026-07-11 16:37:12 +0100

    Modular pathway builder component

---
+++ Gamp-SCPeriop.Shared/Entity/Model/BuilderComponentDto.cs
@@ -0,0 +1,18 @@
+﻿using GamP_SCPeriop.Shared.Enum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamP_SCPeriop.Shared.Entity.Model
+{
+    public class BuilderComponentDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public ModuleStage Stage { get; set; }
+        public int Weight { get; set; } = 1;
+        public int? ParentComponentId { get; set; }
+        public string? PdfFilePath { get; set; }
+    }
+}
---
+++ Gamp-SCPeriop.Shared/Entity/Model/BuilderModuleDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamP_SCPeriop.Shared.Entity.Model
+{
+    public class BuilderModuleDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int Weight { get; set; } = 1;
+
+        // A lista universal de componentes
+        public List<BuilderComponentDto> Components { get; set; } = new();
+
+        // Mantemos a timeline aqui. Se for Template, o componente simplesmente ignora isto.
+        public List<ModuleStageTimeline>? StageTimelines { get; set; }
+    }
+}
